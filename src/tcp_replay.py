749e919cf9a71f0977de0bb804bda6d655a0a6b4
Jared Welsh <jwelsh1@umd.edu>
2019-04-17 09:33:16 -0400

    updating

--- src/tcp_replay.py
+++ src/tcp_replay.py
@@ -17,19 +17,34 @@ def sniffer(src, dst):
 
 
 # perform triple handshake with destination
-def t_shake(client, src, dst):
+def t_shake(client, src, dst, s_to_d, d_to_s):
     l0, l1, l2 = client.gen_layers(src=src, dst=dst, tcp=True)
-    msg = l0 \ l1 \ l2
+    msg = l0 / l1 / l2
 
-    client.add_options(msg, {'TCP': {'flags': 'S'}})
+    client.add_options(msg, {
+        'TCP': {
+            'flags': 'S',
+            'seq': s_to_d.pop(0)[TCP].seq
+            }})
 
     sendp(msg, iface=client.intface)
 
     t = sniffer(src, dst)
 
-    client.add_options(msg, {'TCP': {'flags': 'SA'}})
+    if 'SA' not in t.flags:
+        print('ERROR: SYN-ACK not received')
+        return False
 
-    t = sniffer(src, dst)
+    msg = l0 / l1 / l2
+
+    client.add_options(msg, {
+        'TCP': {
+            'flags': 'A',
+            'seq': t[TCP].ack,
+            'ack': t[TCP].seq + 1
+            }})
+
+    return True
 
 
 # spoof arp so that other networks recognize spoofed client
@@ -63,4 +78,5 @@ def tcp_replay(client, src, dst):
     s_to_d, d_to_s = pcap_filter(client.pcaps[pcap], src, dst)
 
     arp_poison(client, src, dst)
-    t_shake(client, src, dst)
+    if t_shake(client, src, dst, s_to_d, d_to_s):
+        continue
