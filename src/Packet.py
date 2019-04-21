5d2ca62940b0f6c5e554ce72e25e987994b5ced1
Jared Welsh <jwelsh1@umd.edu>
2019-04-21 12:39:02 -0400

    working on IPv4 header creation

--- src/Packet.py
+++ src/Packet.py
@@ -2,5 +2,18 @@ from layers import *
 
 
 class Packet():
-    def __init__(self):
-        pass
+
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
+        self.msg = None
+
+    def gen_message(self):
+        self.l0 = Ether.Ether(src=self.src.mac, dst=self.dst.mac)
+        self.l1 = IPv4.IPv4(src=self.src.ip, dst=self.dst.ip)
+        return self.l0 + self.l1
+
+    def dummy_gen(self):
+        self.l0 = Ether.Ether(dst="08:00:27:c6:d8:cf", src="08:00:27:7d:9b:7e")
+        self.l1 = IPv4.IPv4(src="192.168.56.3", dst="192.168.56.5")
+        return self.l0 + self.l1
--- src/layers/IPv4.py
+++ src/layers/IPv4.py
@@ -1,23 +1,42 @@
-from socket import inet_aton, IPPROTO_TCP, IPPROTO_UDP
+from socket import inet_aton, inet_ntoa
 from struct import pack, unpack
 
 
 class IPv4():
-    def __init__(self):
-        self.id = None
-        self.ihl = None
-        self.ver = None
-        self.tos = None
-        self.len = None
-        self.frg = None
-        self.ttl = None
-        self.src = None
-        self.dst = None
-        self.protr = None
-        self.check = None
+    ProtoType = {'tcp': 6, 'udp': 17}
+
+    def __init__(self, iden=1000, ihl=5, ver=4, tos=0, leng=0, frg=0, ttl=255,
+                 src='127.0.0.1', dst='127.0.0.1', proto='tcp', check=0,
+                 byte=None):
+        if byte:
+            self.build_from_byte(byte)
+        else:
+            self.iden = iden
+            self.ihl = ihl
+            self.ver = ver
+            self.tos = tos
+            self.leng = leng
+            self.frg = frg
+            self.ttl = ttl
+            self.src = src
+            self.dst = dst
+            self.proto = ProtoType[proto]
+            self.check = check
+
+    def __str__(self):
+        ret = 'IPv4: \n'
+        for k, v in self.__dict__.items():
+            ret += '\t{}: {}\n'.format(k, v)
+        return ret[:-1]
+
+    def build_from_byte(self, s):
+        return
 
     def gen_message(self):
-        return pack('!BBHHHBBH4s4s')
+        return pack('!BBHHHBBH4s4s', ((self.ver << 4) + self.ihl), self.tos,
+                    self.len, self.id, self.frg, self.ttl,
+                    self.proto, self.check, addr_to_bytes(self.src),
+                    addr_to_byte(self.dst))
 
     def addr_to_bytes(self, addr):
         return inet_aton(addr)
